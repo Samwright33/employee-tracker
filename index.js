6137ff94f93d7357ba5e065a39b911e62809aa36
Sam Wright <samwright@Sams-MacBook-Pro-3.local>
2023-05-06 16:53:03 -0500

    Added connections and index logic and readme

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// Dependencies
 const inquirer = require("inquirer");
-const db = require("./config/connection");
+const express = require("express");
+const mysql = require("mysql2");
+const data = require("./config/connection");
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Connecting to database 
+data.connect(err => {
+    if (err) throw err;
+    console.log("Database is connected");
+    employee_tracker();
+});
